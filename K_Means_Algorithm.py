48c834aef9516c86cfafb6079eb6b44dd7eab8e8
Renel Chesak <rchesak1@gmail.com>
2017-05-18 17:06:07 -0500

    Update K_Means_Algorithm.py

--- K_Means_Algorithm.py
+++ K_Means_Algorithm.py
@@ -10,7 +10,7 @@ Created on Thu May 18 16:47:02 2017
 ############################################### 
 import numpy as np
 
-'''This is a set of vectorized algorithms that uses a document-term numpy array to create K-Means cluster centroids and cluster 
+'''This is a set of vectorized algorithms that use a document-term numpy array to create K-Means cluster centroids and cluster 
 assignments.
 These functions will allow you to cluster your documents, then use the model centroids in a classifer algorithm to classify 
 unknown documents.'''
