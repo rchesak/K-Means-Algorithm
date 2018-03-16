ffaa1d0a0ec290eda8e7752ba5f5cc53c56ef8bf
Renel Chesak <rchesak1@gmail.com>
2018-03-16 11:15:19 +0100

    Updates to notation
    Made notation more readable.

--- K_Means_Algorithm.py
+++ K_Means_Algorithm.py
@@ -2,18 +2,18 @@
 """
 Created on Thu May 18 16:47:02 2017
 
-@author: Renel
+@author: Renel Chesak
 """
-
-###############################################
-# Import required libraries:
-############################################### 
-import numpy as np
 
 '''This is a set of vectorized algorithms that use a document-term numpy array to create K-Means cluster centroids and cluster 
 assignments.
 These functions will allow you to cluster your documents, then use the model centroids in a classifer algorithm to classify 
 unknown documents.'''
+
+###############################################
+# Import required libraries:
+############################################### 
+import numpy as np
 
 ###############################################
 # Euclidean Distance Algorithm:
@@ -26,7 +26,6 @@ def distEclud(vecA, vecB):
 # Cosine Similarity Algorithm:
 ############################################### 
 
-#cosine similarity between 2 vectors:
 #cosine similarity between 2 vectors:
 def cosine_sim(vecA, vecB):
     #create normalized copies of each vector:
@@ -42,15 +41,15 @@ def cosine_sim(vecA, vecB):
 
 #create K random centers in your dataset to be used in your clustering algorithm:
 def randCent(dataSet, k):
+    # Where dataSet is a Numpy array, and k is the desired number of clusters.
     n = np.shape(dataSet)[1] #n will be the number of terms
     centroids = np.zeros((k,n), dtype=float) #creates a matrix of zeros with the shape:K rows by n columns
     for j in range(n): #iterate once for each term
         minJ = min(dataSet[:,j]) #minimum value in that term's column
         rangeJ = float(max(dataSet[:,j]) - minJ) #range of values in that term's column
         centroids[:,j] = minJ + rangeJ * np.random.rand(k) #set each row in that term's column to a different random number
-        # that has been multiplied by the sum of min and range (this must be some way of normalizing the center so that it will
-        #be somewhat close to the actual data points)
-    centroids = np.array(centroids)
+        # that has been multiplied by the sum of min and range as a way of normalizing the random centers
+    centroids = np.array(centroids) # ensure the matrix is a 2D Numpy array, as these are extremely fast for matrix manipulation
     return centroids 
 
 ###############################################
@@ -58,6 +57,10 @@ def randCent(dataSet, k):
 ############################################### 
 
 def K_Means(dataSet, k, max_iter=300, distMeas=cosine_sim, createCent=randCent):
+    # Where dataSet is a Numpy array, k is the desired number of clusters, max_iter is the maximum number of loops (to avoid large or
+    # infinite iterations), distMeas is the desired similarity measure (cosine similarity or Euclidean distance), and createCent is
+    # the function which creates the initial randomized cluster centers.
+    
     # Store the number of documents
     docs = np.shape(dataSet)[0]
     
@@ -71,55 +74,51 @@ def K_Means(dataSet, k, max_iter=300, distMeas=cosine_sim, createCent=randCent):
     clusterChanged = True
     cur_iter = 0
     
-    # While the clusters are changing and we have not reached the max iteration:
+    # While the cluster assignments are still changing and we have not reached the max iteration:
     while clusterChanged and cur_iter < max_iter:
-        # Update the clusterChanged value so we don't run into an infinite loop. DO YOU REALLY NEED THIS since there is a break?
+        # Update the clusterChanged value so we don't run into an infinite loop.
         clusterChanged = False # note that there is code further down to change this to true if the clusters did change
+        
         #assign the previous cluster to an object:
-        previous_cluster = np.array(cluster, copy=True)
+        previous_cluster = np.array(cluster, copy=True) #intially, this will be the random cluster starting points
+        
         # Create an intital matrix of zeros to store similarity values:
         similarity = np.zeros((k,docs)) #this will eventually show you how similar each doc is to each cluster's centroid
         for j in range(k): #iterate once for each cluster (row)
-            # Run cosine similarity as a numpy function along axis 1 
-            #(updates each doc's similarity value for one cluster at a time):
-            similarity[j,:] = np.apply_along_axis(distMeas, 1, dataSet, Centroids[j,:])
-
-#similarity[j,] = np.apply_along_axis(func1d=distMeas, axis=1, arr=dataSet, args=Centroids[j,:])
-# numpy.apply_along_axis(func1d, axis, arr, *args, **kwargs)      
-# Apply a function to 1-D slices along the given axis.
-# Execute func1d(a, *args) where func1d operates on 1-D arrays and 'a' is a 1-D slice of 'arr' along axis.         
-        
-        # Max values = most similar. Capture both the cluster assigment and the similarity value
-        cluster[:,0] = np.argmax(similarity, axis=0) #'np.argmax' returns the indices of the maximum similarity values for each
-        #doc => the cluster it is most similar to
-        #'cluster[:,0]' => for each row in the first column, value = row index of the most similar cluster (row index => cluster)
-        
-        cluster[:,1] = np.amax(similarity, axis=0) #'np.amax' returns the maximum value of an array or maximum along an axis.
-        #'cluster[:,1]' => for each row in the second column, value = max sim value for that document
+            # Run cosine similarity as a numpy function along axis 1 (updates each doc's similarity value, one cluster at a time):
+            similarity[j,:] = np.apply_along_axis(func1d=distMeas, axis=1, dataSet, Centroids[j,:])
+            # Documentation: 
+            # Execute func1d(a, *args) where func1d operates on 1-D arrays and 'a' is a 1-D slice of 'arr' along axis.       
+            # Apply a function to 1-D slices along the given axis.
+       
+        # Capture both the cluster assigment and the similarity value. Note: max values => most similar. 
+        cluster[:,0] = np.argmax(similarity, axis=0)         
+        cluster[:,1] = np.amax(similarity, axis=0) 
         
         # If the clusters have not changed, break out of while loop
         if np.array_equal(cluster, previous_cluster):
-            break #breaks the while loop if the cluster assinments haven't changed
+            break 
         else:
             clusterChanged = True #sets this back to true if the cluster assinments are still changing
-            # We only bother to recalculate centroid if there was a reassignment
-            for centroid in range(k):
-                # Find the documents in each cluster
-                correct_cluster = np.where(cluster[:,0] == centroid)[0]
-                #'np.where' return elements, either from x or y, depending on condition.
-                # Take the documents in each cluster
-                docs_in_cluster = np.take(dataSet, correct_cluster, axis=0)
-                # If more than one document, take the mean as the new centroid
-                if len(docs_in_cluster) > 0:
+            # We only bother to recalculate centroid if there was a cluster reassignment
+            for centroid in range(k): #iterate once for each cluster
+                # Find the indicies of the documents that make up each cluster
+                correct_cluster = np.where(cluster[:,0] == centroid)[0] 
+                # Use the indicies to grab and store those documents separately
+                docs_in_cluster = np.take(dataSet, correct_cluster, axis=0) 
+                # If more than one document, take the mean as the new centroid (otherwise centroid is that one document)
+                if len(docs_in_cluster) > 0: # TEST THIS CODE to see if the if statement can be removed
                     Centroids[centroid,:] = np.mean(docs_in_cluster, axis=0)
         cur_iter += 1
     return Centroids, cluster
 
 ###############################################
-# Vectorized Classifier based upon the K_Means Model:
- ###############################################   
+# Vectorized Classifier (based upon the K_Means Model):
+###############################################   
 
-def K_Means_Model_Classifier2(centroids, dataSet, max_iter=300, distMeas=cosine_sim):
+def K_Nearest_Neighbor_Classifier(centroids, dataSet, max_iter=300, distMeas=cosine_sim):
+    # Where centroids are those found from running K_Means(), dataSet is a Numpy array, max_iter is the maximum number of loops (to 
+    # avoid large or infinite iterations), and distMeas is the desired similarity measure (cosine similarity or Euclidean distance).
     
     docs = dataSet.shape[0]
     k = centroids.shape[0]
@@ -130,30 +129,25 @@ def K_Means_Model_Classifier2(centroids, dataSet, max_iter=300, distMeas=cosine_
     # Create an intital matrix of zeros to store similarity values:
     similarity = np.zeros((k,docs))
     
-    for j in range(k): #iterate once for each cluster (row in 'similarity')
-        # Run cosine similarity as a numpy function along axis 1 
-        #(updates each doc's similarity value for one cluster at a time):
-        similarity[j,:] = np.apply_along_axis(distMeas, 1, dataSet, centroids[j,:])
+    for j in range(k): #iterate once for each cluster
+        # Run your similarity function along axis 1 (updates each doc's similarity value, one cluster at a time):
+        similarity[j,:] = np.apply_along_axis(func1d=distMeas, axis=1, dataSet, centroids[j,:])
     
-    # Max values = most similar. Capture both the cluster assigment and the similarity value
-    cluster[:,0] = np.argmax(similarity, axis=0) #'np.argmax' returns the indices of the maximum similarity values for each
-    #doc => the cluster it is most similar to
-    #'cluster[:,0]' => for each row in the first column, value = row index of the most similar cluster (row index => cluster)
-
-    cluster[:,1] = np.amax(similarity, axis=0) #'np.amax' returns the maximum value of an array or maximum along an axis.
-    #'cluster[:,1]' => for each row in the second column, value = max sim value for that document
+    # Capture both the cluster assigment and the similarity value. Note: max values => most similar. 
+    cluster[:,0] = np.argmax(similarity, axis=0)
+    cluster[:,1] = np.amax(similarity, axis=0)
     
-    #transpose similarity to make it doc-cluster:
+    # Transpose similarity to make it a document-by-cluster matrix:
     similarity_T = similarity.T
     
-    #create a slice of cluster that just contains the assignments:
+    # Create a slice of cluster that just contains the assignments:
     cluster_assignments = cluster[:, 0]
     
-    #make the cluster_assignments 2D:
+    # Make the cluster_assignments a 2D Numpy array:
     cluster_assignments_reshaped = cluster_assignments.reshape(-1, 1)
 
-    #create an array where the rows are the docs, the first column is the cluster assignment, and the next K columns are the 
-    #similarities to each cluster:
+    # Create an array where the rows are the docs, the first column is the cluster assignment, and the next K columns are the 
+    # similarities to each cluster:
     all_results = np.concatenate((cluster_assignments_reshaped, similarity_T), axis=1)
     
     return all_results, cluster_assignments
